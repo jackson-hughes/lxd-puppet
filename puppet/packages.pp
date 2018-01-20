ad667ec306c0f3f36c65cb050a856f90ff20a7ab
Jackson Mark Hughes <jhughes@Jacksons-MBP.jhcloud.io>
2018-01-20 22:32:38 +0000

    added inspec (inc. dependencies)  & puppet-lint to installed packages

--- puppet/packages.pp
+++ puppet/packages.pp
@@ -1,5 +1,15 @@
-$packages = ['git', 'curl']
+$packages = ['ruby', 'ruby-dev', 'gcc', 'make']
 
 package { $packages:
-   ensure => "installed"
+  ensure => 'installed'
+}
+
+package { 'inspec':
+  ensure   => 'installed',
+  provider => 'gem'
+}
+
+package { 'puppet-lint':
+  ensure   => 'installed',
+  provider => 'gem',
 }
